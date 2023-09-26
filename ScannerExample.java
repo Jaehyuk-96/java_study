10b2c7fdd0c76d15715ae0f15e0f99e9a88dd247
CJH <jaehyuck000@naver.com>
2023-09-26 17:22:06 +0900

    Scanner 사용 예제

---
+++ ScannerExample.java
@@ -0,0 +1,34 @@
+package com.mystudy.scanner;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("x값 입력:");
+        String strx = scanner.nextLine();
+        int x = Integer.parseInt(strx);
+
+        System.out.println("y값 입력:");
+        String stry = scanner.nextLine();
+        int y = Integer.parseInt(stry);
+
+        while (true){
+            System.out.println("입력 문자열:");
+            String data = scanner.nextLine();
+            if(data.equals("q")){
+                break;
+            }
+            System.out.println("출력문자열:" + data);
+            System.out.println();
+
+
+
+        }
+
+        System.out.println("종료");
+
+    }
+
+}
