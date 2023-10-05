52be0235c5fb6c2c133a32769d6bf172bcf2d73b
CJH <jaehyuck000@naver.com>
2023-10-05 17:32:40 +0900

    args 입력값 주기(run configuration, cmd 활용)

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.mystudy.oct05;
+
+public class Main {
+    public static void main(String[] args) {
+        for(String arg:args){
+            System.out.println(arg);
+
+        }
+        //edit configuration 들어가서 값입력
+        // args로 전달할 값을 공백으로 구분하여 입력
+        // 공백이 있는 문자열을 전달할 때는 큰따옴표("")로 감싸기
+        //cmd 활용해서도 가능 class 파일을 만들고 java 파일명 값입력
+
+
+
+
+
+    }
+}
