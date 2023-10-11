540bf46a78f4bbee7dc8d66a37e7173c386c350a
CJH <jaehyuck009603@gmail.com>
2023-10-11 17:27:49 +0900

    static final 기호상수 선언

---
+++ Earth.java
@@ -0,0 +1,14 @@
+package oct11;
+
+public class Earth {
+
+    static final double EARTH_RADIUS = 6400;
+
+    static final double EARTH_SURFACE_AREA;
+
+    static{
+        EARTH_SURFACE_AREA = 4 *Math.PI*EARTH_RADIUS*EARTH_RADIUS;
+    }
+
+    //static final 기호상수 선언
+}
