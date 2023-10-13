61aae160d58b29ba6fc579e7b371b6bc530663a6
CJH <jaehyuck009603@gmail.com>
2023-10-13 17:51:26 +0900

    ClassNotFoundException 활용

---
+++ ExceptionHandlingExample3.java
@@ -0,0 +1,20 @@
+package oct13;
+
+public class ExceptionHandlingExample3 {
+    public static void main(String[] args) {
+        try{
+            Class.forName("java.lang.String");
+            System.out.println("java.lang.String 클래스가 존재합니다.");
+        }catch(ClassNotFoundException e){
+    e.printStackTrace();
+        }
+        System.out.println();
+        try{
+            Class.forName("java.lang.String2");
+            System.out.println("java.lang.String2 클래스가 존재합니다.");//String2 라는 클래스 존재하지 않음
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
