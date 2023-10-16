250a1ae9d5997ff1603882281c00edf1bbfec9f3
CJH <jaehyuck009603@gmail.com>
2023-10-16 17:50:04 +0900

    Date 사용해서 날짜시간연도 출력해보기

---
+++ DateExample.java
@@ -0,0 +1,17 @@
+package oct16;
+
+import java.text.*;//3개 이상일때만 사용
+import java.util.*;
+
+public class DateExample {
+    public static void main(String[] args) {
+        Date now = new Date();
+        String strNow1 = now.toString();
+        System.out.println(strNow1);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String strNow2 = sdf.format(now);
+        System.out.println(strNow2);
+
+    }
+}
