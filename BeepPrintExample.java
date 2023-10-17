4f77211e884c4e84c5493d404890d5e086a71310
CJH <jaehyuck009603@gmail.com>
2023-10-17 16:49:45 +0900

    메인 스레드가 동시에 두가지 작업을 처리할 수 없음을 보여주는 예제

---
+++ BeepPrintExample.java
@@ -0,0 +1,21 @@
+package oct17.thread1;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for(int i = 0; i < 5; i++) {
+            toolkit.beep();
+            try {Thread.sleep(500);
+            } catch(Exception e) {}
+            }
+            for(int i = 0; i < 5; i++) {
+                System.out.println("띵");
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {}
+            }
+        }
+    }
+
