1216f6c8c454fd8ebeb8bc09a856bc23de695f09
CJH <jaehyuck009603@gmail.com>
2023-10-18 17:33:46 +0900

    ArrayLit와 향상된 for문을 활용하여 객체 가져오기

---
+++ Board.java
@@ -0,0 +1,17 @@
+package com.mystudy.oct18;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+
+
+public class Board {
+    private String subject;
+    private String content;
+    private String writer;
+
+}
