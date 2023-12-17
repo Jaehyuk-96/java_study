a5bfd856ddaf4a5f8e5e0c0fecbdd2e1797eb3be
Jae <jaehyuck009603@gmail.com>
2023-12-17 23:26:47 +0900

    final과 상수를 이용한 예제

---
+++ final1/Constant.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class Constant {
+    //final 앞에 static을 붙이는게 메모리에 효율적이다
+    //수학 상수
+    public static final double PI = 3.14;
+    //시간 상수
+    public static final int HOURS_IN_DAY = 24;
+    public static final int MINUTES_IN_HOUR = 60;
+    public static final int SECONDS_IN_MINUTE = 60;
+    //애플리케이션 설정 상수
+    public static final int MAX_USERS = 1000;
+}
